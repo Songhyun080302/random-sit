bbf3268d2605df1a8cc5206bb98e3b3cddef982e
Songhyun080302 <songhyun080302@naver.com>
2025-01-27 18:14:52 +0900

    Update ChongtianMaandHaokaiMa.py

--- ChongtianMaandHaokaiMa.py
+++ ChongtianMaandHaokaiMa.py
@@ -1,19 +1,17 @@
 def solve():
-    N = int(input())  
-    a = list(map(int, input().split()))
-    b = list(map(int, input().split()))  
-    
-    checked_initial = [1 if a[i] == b[i] else 0 for i in range(
+    N = int(input())  # Number of cows
+    a = list(map(int, input().split()))  # Species of the cows
+    b = list(map(int, input().split()))  # Required species for checkup
+    checked_initial = [1 if a[i] == b[i] else 0 for i in range(N)]
     result = [0] * (N + 1)
     for l in range(N):
         current_checked = checked_initial[:]
         for r in range(l, N):
+            
             current_checked[l:r+1] = reversed(current_checked[l:r+1])
-        
+            
             checked_count = sum(current_checked)
-    
             result[checked_count] += 1
-
+    
     for count in result:
         print(count)
-
