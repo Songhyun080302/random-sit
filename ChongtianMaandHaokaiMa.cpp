d3b36dc2c6221bfb011c5086d08dda693c436c25
Songhyun080302 <songhyun080302@naver.com>
2025-01-27 18:26:44 +0900

    Update ChongtianMaandHaokaiMa.cpp

--- ChongtianMaandHaokaiMa.cpp
+++ ChongtianMaandHaokaiMa.cpp
@@ -15,31 +15,32 @@ int main() {
         cin >> b[i];
     }
 
-    vector<int> count(N + 1, 0); 
-    for (int l = 1; l <= N; l++) {
-        for (int r = l; r <= N; r++) {
-            int checked = 0;
-            
+    vector<int> count(N + 1, 0);  
+
+     for (int l = 1; l <= N; l++) {
+        for (int r = l; r <= N; r++) {  
+            int checked = 0;  
+
             for (int i = 0; i < l - 1; i++) {
                 if (a[i] == b[i]) {
                     checked++;
                 }
             }
-            for (int i = l - 1, j = r - 1; i < r; i++, j--) { // i < r 조건으로 변경
+
+            for (int i = l - 1, j = r - 1; i < r; i++, j--) {
                 if (a[j] == b[i]) {
                     checked++;
                 }
             }
+
             for (int i = r; i < N; i++) {
                 if (a[i] == b[i]) {
                     checked++;
                 }
             }
-
             count[checked]++;
         }
     }
-
     for (int i = 0; i <= N; i++) {
         cout << count[i] << endl;
     }
